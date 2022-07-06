459ec9506abab048180c7db544c1598accda9c6f
yfktn <friskantoni@gmail.com>
2022-07-06 20:16:06 +0700

    Final dulu report laporan

--- controllers/RenderLaporan.php
+++ controllers/RenderLaporan.php
@@ -4,6 +4,9 @@ use Backend\Classes\Controller;
 use BackendMenu;
 use Db;
 use Exception;
+use Illuminate\Http\Response;
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Contracts\Container\BindingResolutionException;
 use October\Rain\Exception\ApplicationException;
 use October\Rain\Support\Facades\Flash;
 use Yfktn\LaporanKependudukan\Models\Desa;
@@ -54,7 +57,7 @@ class RenderLaporan extends Controller
             throw new ApplicationException("Periode Tahun Tidak Didefinisikan, mohon pilih dahulu periode tahunnya!");
         }
         if(empty($pilihanUser['desa'])) {
-            throw new ApplicationException("Desa belum dipilih! Mohon dipilih dulu desanya.");
+            throw new ApplicationException("Jenis laporan ini membutuhkan data desa, dan desa belum dipilih! Mohon dipilih dulu desanya.");
         }
         $totals = [];
         $sql = <<<SQLQUERY
@@ -81,7 +84,7 @@ SQLQUERY;
         ];
     }
 
-    public function downloadLaporanDesaBerdasarkanPeriode($desaId, $periodeTahun)
+    public function downloadLaporanDesaBerdasarkanPeriode($periodeTahun, $desaId = null)
     {
         $this->layout = "report";
         return response(
@@ -151,12 +154,11 @@ SQLQUERY;
      * @throws ApplicationException 
      * @throws Exception 
      */
-    protected function summary_laporan_desa($pilihanUser)
+    protected function summary_laporan_desa($pilihanUser, $tampilDownload = false)
     {
         if(empty($pilihanUser['periode_tahun'])) {
             throw new ApplicationException("Periode Tahun Tidak Didefinisikan, mohon pilih dahulu periode tahunnya!");
         }
-        $bulan = range(1, 12);
         $parameters = [];
         $parameters[] = $pilihanUser['periode_tahun'];
         $sqlQuerySummary = <<<QRY
@@ -196,11 +198,38 @@ QRY;
         $this->vars['dataTable'] = $dataTable;
         $this->vars['periodeTahun'] = $pilihanUser['periode_tahun'];
         $this->vars['desaId'] = $pilihanUser['desa'];
+        $this->vars['tampilDownload'] = $tampilDownload;
+
+        if($tampilDownload) {
+            return $this->makePartial('render_summary_laporan_desa');
+        }
 
         Flash::success("Report telah dirender, silahkan check pada tab Hasil.");
         return [
             '#hasilRender' => $this->makePartial('render_summary_laporan_desa')
         ];
+    }
+
+    /**
+     * User memilih untuk melakukan download terhadap laporannya!
+     * @param mixed $desaId 
+     * @param mixed $periodeTahun 
+     * @return Response|ResponseFactory 
+     * @throws ApplicationException 
+     * @throws Exception 
+     * @throws BindingResolutionException 
+     */
+    public function downloadSummaryLaporanDesa($periodeTahun, $desaId = null)
+    {
+        $this->layout = "report";
+        return response(
+            $this->summary_laporan_desa([ 'desa'=>$desaId, 'periode_tahun' => $periodeTahun], true), 
+            200,
+            [
+                'Content-Type' => 'application/vnd-ms-excel',
+                'Content-Disposition' => 'attachment; filename=laporan-summary-desa.xls'
+            ]
+        );
     }
 
     /**
